de68e5825040fabd9ae15f54c8556b709f515b7b
张奕 <mowozy@foxmail.com>
2017-12-12 14:46:10 +0800

    YESdemo

--- src/main/java/Dao/Users.java
+++ src/main/java/Dao/Users.java
@@ -1,9 +1,10 @@
 package Dao;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by moonwolfzy on 2017-12-12.
  */
 
 
-public interface Users {
+public interface Users extends JpaRepository{
 }
