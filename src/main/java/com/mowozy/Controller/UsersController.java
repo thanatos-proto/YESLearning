ce731c141134abbc28b5909dd9e357f5a77a63ea
chenjun <437456560@qq.com>
2018-01-05 03:00:32 +0800

    登陆后显示登录状态。暂时只修改了index等3个页面，其它页面改法类似。

--- src/main/java/com/mowozy/Controller/UsersController.java
+++ src/main/java/com/mowozy/Controller/UsersController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by moonwolfzy on 2017-12-12.
@@ -19,80 +23,88 @@ import javax.servlet.http.HttpServletRequest;
 @EnableAutoConfiguration
 public class UsersController {
 
-    @Autowired
-    private UsersService usersService;
+	@Autowired
+	private UsersService usersService;
 
-    @RequestMapping("/login")
-    String login(Model model) {
-        model.addAttribute("users", new Users());
-        return "login";
-    }
+	@RequestMapping("/login")
+	String login(Model model) {
+		model.addAttribute("users", new Users());
+		return "login";
+	}
 
-    @RequestMapping("/register")
-    String register(Model model) {
-        model.addAttribute("users", new Users());
-        return "register";
-    }
+	@RequestMapping("/register")
+	String register(Model model) {
+		model.addAttribute("users", new Users());
+		return "register";
+	}
 
+	@RequestMapping(value = "/userLogin", method = RequestMethod.POST)
+	String userLogin(Users user, HttpServletRequest request, HttpServletResponse response) {
+		boolean flag = usersService.verifyUser(user);
+		if (flag) {
+			response.addCookie(new Cookie("username", usersService.getUnameByEmail(user.getEmail())));
+			response.addCookie(new Cookie("password", user.getPassword()));
 
-    @RequestMapping(value = "/userLogin", method = RequestMethod.POST)
-    String userLogin(Users user, Model model,HttpServletRequest request) {
+			request.getSession().setAttribute("userid", user.getuId());
+			return "userspace-details";
+		} else {
+			request.setAttribute("error", "用户名或密码错误");
+			return "login";
+		}
+	}
 
-        boolean flag = usersService.verifyUser(user);
-        if (flag) {
-            request.getSession().setAttribute("userid", user.getuId());
-            return "userspace-details";
-        } else {
-            request.setAttribute("error","用户名或密码错误");
-            return "login";
-        }
+	@RequestMapping(value = "/userLogout", method = RequestMethod.GET)
+	String userLogout(HttpServletResponse response) {
+		Cookie name_cookie = new Cookie("username", "");
+		Cookie pass_cookie = new Cookie("password", "");
 
-    }
+		name_cookie.setMaxAge(0);
+		pass_cookie.setMaxAge(0);
 
+		response.addCookie(name_cookie);
+		response.addCookie(pass_cookie);
 
+		return "index";
+	}
 
-    @RequestMapping(value = "/userRegister", method = RequestMethod.POST)
-    String registerUser(Users user, Model model) {
+	@RequestMapping(value = "/userRegister", method = RequestMethod.POST)
+	String registerUser(Users user, Model model) {
+		boolean flag = usersService.registerUser(user);
+		if (flag) {
+			model.addAttribute("name", user.getuName());
+			return "registerSuccess";
+		} else {
+			model.addAttribute("email", user.getEmail());
+			return "registerFail";
+		}
+	}
 
+	@RequestMapping("userspace")
+	String userspace(Model model) {
+		return "userspace";
+	}
 
-        boolean flag = usersService.registerUser(user);
-        if (flag) {
-            model.addAttribute("name",user.getuName());
-            return "registerSuccess";
+	@RequestMapping("userspace-details")
+	String userspaceDetails(Model model) {
 
-        }
-        else{
-            model.addAttribute("email",user.getEmail());
-            return "registerFail";
-        }
+		return "userspace-details";
+	}
 
-    }
+	@RequestMapping("userspace-modify")
+	String userspacemodify(Model model) {
 
-    @RequestMapping("userspace")String userspace(Model model){
-        return "userspace";
-    }
+		return "userspace-modify";
+	}
 
-    @RequestMapping("userspace-details")String userspaceDetails(Model model){
+	@RequestMapping("userspace-courses")
+	String userspaceCourses(Model model) {
+		return "userspace-courses";
+	}
 
-        return "userspace-details";
-    }
-
-    @RequestMapping("userspace-modify")String userspacemodify(Model model){
-
-        return "userspace-modify";
-    }
-    @RequestMapping("userspace-courses")String userspaceCourses(Model model)
-    {
-        return "userspace-courses";
-    }
-
-    @RequestMapping("userspace-exam")String userspaceExam(Model model)
-    {
-        return "userspace-exam";
-    }
-
-
-
+	@RequestMapping("userspace-exam")
+	String userspaceExam(Model model) {
+		return "userspace-exam";
+	}
 }
 
 
